f8de344c44ee7d516c2fa907c65d6de801220ef5
R0y3r929 <rogelio.senovio.romero@hotmail.com>
2024-10-11 17:46:52 -0600

    Update de Recomendaciones y de Bd

--- src/routes/Inicio.jsx
+++ src/routes/Inicio.jsx
@@ -10,8 +10,7 @@ const Inicio = () => {
         </div>
         <div className='content-text-descript'>
           <span className='text-descript'>🔸Aqui podras consultar el status de tu credencial recuerda tener a la mano tu matricula</span>
-          <span className="text-descript">🔸Trata de que el horario en que consultes el status de tu tramite sea de 9am a 7pm</span>
-          <span className="text-descript">🔸Los status de las credenciales se actualizan todo los lunes despues de las 10am</span>
+          <span className="text-descript">🔸El status de las credenciales se actualizan todos los lunes despues de las 10am</span>
           <span className='text-descript'>🔸En caso de no haber iniciado tramite no podras darle seguimiento a tu status </span>
           <span className="text-descript">🔸Si aun no has iniciado Tramite y quieres cambiar fotografia <a href="https://forms.gle/4z7WfsjcSU67oCxM7">pulsa aqui</a></span>
         </div>
