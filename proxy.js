7423f4428fe652e8b43a099a1a2f5ecb3a059b93
R0y3r929 <rogelio.senovio.romero@hotmail.com>
2024-10-11 15:07:11 -0600

    Se actualiza la direccion de la api

--- proxy.js
+++ proxy.js
@@ -2,5 +2,5 @@ const http = require('http');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use('/api', createProxyMiddleware({ target: 'http://upemtexcoco.ddns.net:3000', changeOrigin: true }));
+  app.use('/api', createProxyMiddleware({ target: 'http://upemtexcoco.ddns.net:3000/api/credenciales/getCredencialAlumn/', changeOrigin: true }));
 };
