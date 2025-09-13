c1154756f77f0ff4ed0f9320ad11838aabcc0c90
R0y3r929 <rogelio.senovio.romero@hotmail.com>
2025-09-12 19:16:31 -0600

    Refactor: Files Solicitud

--- src/Constantes.js
+++ src/Constantes.js
@@ -1,6 +1,6 @@
 const Constantes = {
     RUTA_API_GLOBAL: "http://upemtexcoco.ddns.net:3000",
     RUTA_GOOGLE_DRIVE: "https://script.google.com/macros/s/AKfycbyr3MlhhCKHgvlcrdyBr_kjRtY-5Uxw9Flmv_uI136XdXCF0Ga0EijcIaUORyudqF4g/exec",
-    RUTA_UPLOAD_PHOTO: "https://script.google.com/macros/s/AKfycby5CX5S3vRlz18xHnhEa9xj2iHi2OCmmFKE2y-BdNmMt2anmu9Zea6xlpFkSOVHTr-a/exec",
+    RUTA_UPLOAD_PHOTO: "https://script.google.com/macros/s/AKfycbzcI_pMOXXtlonupKUPTi8cMsAAIl_fx2_82TA0jaXJIlcyIKSkMH7fVl2KcfDfUiNCgQ/exec",
 };
 export default Constantes;
