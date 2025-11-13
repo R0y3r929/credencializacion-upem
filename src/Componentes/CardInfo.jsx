9661b8d3f447754b58f33ac999ae971392087581
R0y3r929 <rogelio.senovio.romero@hotmail.com>
2025-11-13 16:16:31 -0600

    solucion

--- src/Componentes/CardInfo.jsx
+++ src/Componentes/CardInfo.jsx
@@ -5,7 +5,7 @@ const CardInfo = ({icon, title, descript}) => {
   return (
     <div className='card-info'>
         <div style={{display: 'flex', gap:10}}>
-            <img src={`../../public/${icon}.png`} alt={icon} className='icon-cardInfo' />
+            <img src={`/${icon}.png`} alt={icon} className='icon-cardInfo' />
             <h3 className='title-info'>{title || 'TITULO'}</h3>
         </div>      
       <p className='descript-info'>{descript}</p>
