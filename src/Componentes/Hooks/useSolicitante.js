c58e7d4f214615ad01cc55f649676c255c66973a
R0y3r929 <rogelio.senovio.romero@hotmail.com>
2026-03-19 14:59:24 -0600

    Refactor buscarSolicitante

--- src/Componentes/Hooks/useSolicitante.js
+++ src/Componentes/Hooks/useSolicitante.js
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 import credenciales from '../../files/credenciales'
 
 const useSolicitante = () => {
-    const [user, setUser] = useState({matricula: '', nombre: '', carrera: '', status: ''});
+    const [user, setUser] = useState({ matricula: '', nombre: '', carrera: '', status: '' });
     const [matchedUser, setMatchedUser] = useState(false);
-    const buscarSolicitante = (matricula) => {
+    /*const buscarSolicitante = (matricula) => {
         const solicitanteEncontrado = credenciales.find((alumno) => alumno.MATRÍCULA === matricula);
         if (solicitanteEncontrado) {
             console.log('Solicitante encontrado:', solicitanteEncontrado);
@@ -18,13 +18,31 @@ const useSolicitante = () => {
             );
             setMatchedUser(true);
         } 
-    };
-    useEffect(()=>{        
-        buscarSolicitante(user.matricula);        
+    };*/
+    const buscarSolicitante = async (matricula) => {
+        const url = `${Constantes.RUTA_API_GLOBAL}/api/credenciales/getCredencialAlumn/${matricula}`;
+        const peticion = await fetch(url)
+        const resp = await peticion.json()
+
+        if (resp.status !== 500) {
+            if (resp.length > 0) {
+                console.log('solicitante encontrado', resp[0])
+                setUser({
+                    matricula: resp[0].matricula,
+                    nombre: resp[0].nombre + ' ' + resp[0].paterno + ' ' + resp[0].materno,
+                    carrera: resp[0].carrera,
+                    status: resp[0].status,
+                })
+                setMatchedUser(true)
+            }
+        }
+    }
+    useEffect(() => {
+        buscarSolicitante(user.matricula);
     }, [user.status]);
 
     return {
-        user, 
+        user,
         setUser,
         matchedUser,
     };
